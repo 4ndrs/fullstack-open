b42753fa0f57d6e7860cb4010536dc8214508fae
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-09 17:34:38 +0100

    Part1: Add `Complex state` modifications

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,24 +1,29 @@
 import { useState } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [clicks, setClicks] = useState({
+    left: 0,
+    right: 0,
+  });
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const setToZero = () => setCounter(0);
-  const decreaseByOne = () => setCounter(counter - 1);
+  const handleLeftClick = () => {
+    const newClicks = { ...clicks, left: clicks.left + 1 };
+    setClicks(newClicks);
+  };
+
+  const handleRightClick = () => {
+    const newClicks = { ...clicks, right: clicks.right + 1 };
+    setClicks(newClicks);
+  };
 
   return (
-    <>
-      <Display counter={counter} />
-
-      <Button onClick={increaseByOne} text="add" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
-    </>
+    <div>
+      {clicks.left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {clicks.right}
+    </div>
   );
 };
-
-const Display = ({ counter }) => <div>{counter}</div>;
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 export default App;
