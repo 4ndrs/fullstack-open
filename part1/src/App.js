288d9d0858f22da58560507045771d3454282f9b
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-09 15:12:47 +0100

    Part1: Add `Passing state to child components` modifications

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,14 +5,25 @@ const App = () => {
 
   const increaseByOne = () => setCounter(counter + 1);
   const setToZero = () => setCounter(0);
+  const decreaseByOne = () => setCounter(counter - 1);
 
   return (
     <>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>plus</button>
-      <button onClick={setToZero}>zero</button>
+      <Display counter={counter} />
+
+      <Button onClick={increaseByOne} text="add" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </>
   );
+};
+
+const Display = ({ counter }) => {
+  return <div>{counter}</div>;
+};
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>;
 };
 
 export default App;
