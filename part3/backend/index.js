631d69dd8e9d156b0561d1fa5e9f027ee29e666c
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-15 00:06:32 +0100

    Part3: Add `Same origin policy and CORS` modifications

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
 const requestLogger = (request, response, next) => {
@@ -9,6 +11,7 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
