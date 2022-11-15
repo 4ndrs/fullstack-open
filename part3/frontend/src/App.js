24544f1060d66078adc39ce9ff2f29863f7a1d78
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-15 14:53:54 +0100

    Add `The whole app to internet` modifications

--- part3/frontend/src/App.js
+++ part3/frontend/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState("some error happened....");
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     noteService.getAll().then((initialNotes) => {
@@ -58,7 +58,7 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
-      <Notification message={errorMessage} />
+      {errorMessage && <Notification message={errorMessage} />}
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? "important" : "all"}
