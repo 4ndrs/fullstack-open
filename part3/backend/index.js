7952682ab0263eaf17034ec6f79a2ad311b198ad
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-16 14:32:42 +0100

    Part3: Add `Backend connected to a database` modifications

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,7 +1,30 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
 
+require("dotenv").config({ path: "./.env.local" });
+
 const app = express();
+
+const password = process.env.DB_PSW;
+const url = process.env.DB_URL.replace("<password>", password);
+
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  date: Date,
+  important: Boolean,
+});
+
+noteSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id, delete returnedObject.__v;
+  },
+});
+
+const Note = mongoose.model("Note", noteSchema);
 
 const requestLogger = (request, response, next) => {
   console.log("Method:", request.method);
@@ -16,33 +39,14 @@ app.use(express.static("build"));
 app.use(express.json());
 app.use(requestLogger);
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true,
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false,
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true,
-  },
-];
-
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
 
 app.get("/api/notes", (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 
 app.get("/api/notes/:id", (request, response) => {
