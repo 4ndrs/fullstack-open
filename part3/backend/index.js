729da20359254b1c6471a7e0c92f38be40e7ca4c
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-11-17 02:13:34 +0100

    Part3: Add `Moving error handling into middleware` modifications

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -28,7 +28,7 @@ app.get("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes/:id", (request, response) => {
+app.get("/api/notes/:id", (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
@@ -37,10 +37,7 @@ app.get("/api/notes/:id", (request, response) => {
         response.status(404).end();
       }
     })
-    .catch((error) => {
-      console.log(error);
-      response.status(400).send({ error: "malformatted id" });
-    });
+    .catch((error) => next(error));
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -71,10 +68,21 @@ app.post("/api/notes", (request, response) => {
 });
 
 const unkownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unkown endpoint" });
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
 };
 
 app.use(unkownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
