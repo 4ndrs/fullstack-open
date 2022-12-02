04a1bf03756baed2309a51ef978ea19a13927162
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-02 18:22:11 +0100

    Part5: Add `Failed login test` modifications

--- part5/cypress/e2e/note_app.cy.js
+++ part5/cypress/e2e/note_app.cy.js
@@ -32,6 +32,20 @@ describe("Note app", function () {
     cy.contains("Kamen Rider Faiz logged-in");
   });
 
+  it.only("login fails with wrong password", function () {
+    cy.contains("login").click();
+    cy.get("#username").type("faiz");
+    cy.get("#password").type("913");
+    cy.get("#login-button").click();
+
+    cy.get(".error")
+      .should("contain", "Wrong credentials")
+      .and("have.css", "color", "rgb(255, 0, 0)")
+      .and("have.css", "border-style", "solid");
+
+    cy.get("html").should("not.contain", "Kamen Rider Faiz logged-in");
+  });
+
   describe("when logged in", function () {
     beforeEach(function () {
       cy.contains("login").click();
