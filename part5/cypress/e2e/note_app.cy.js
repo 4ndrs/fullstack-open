e10cf12c7f421221bf1b59a9c3e92c573db36246
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-02 14:35:12 +0100

    Part5: Add `Writing to a form` modifications

--- part5/cypress/e2e/note_app.cy.js
+++ part5/cypress/e2e/note_app.cy.js
@@ -1,9 +1,25 @@
 describe("Note app", function () {
-  it("front page can be opened", function () {
+  beforeEach(function () {
     cy.visit("http://pureryzen.io:1234");
+  });
+
+  it("front page can be opened", function () {
     cy.contains("Notes");
     cy.contains(
       "Note app, Department of Computer Science, University of Helsinki 2022"
     );
+  });
+
+  it("login form can be opened", function () {
+    cy.contains("login").click();
+  });
+
+  it("user can login", function () {
+    cy.contains("login").click();
+    cy.get("#username").type("faiz");
+    cy.get("#password").type("555");
+    cy.get("#login-button").click();
+
+    cy.contains("Kamen Rider Faiz logged-in");
   });
 });
