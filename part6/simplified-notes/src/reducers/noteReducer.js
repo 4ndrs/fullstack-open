83eb5996708c0073b427e3e2e81bb7b88a290443
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-03 22:21:34 +0100

    Part6: Add `Array spread syntax` modifications

--- part6/simplified-notes/src/reducers/noteReducer.js
+++ part6/simplified-notes/src/reducers/noteReducer.js
@@ -1,7 +1,7 @@
 const noteReducer = (state = [], action) => {
   switch (action.type) {
     case "NEW_NOTE":
-      return state.concat(action.data);
+      return [...state, action.data];
     case "TOGGLE_IMPORTANCE":
       return state.map((note) => {
         if (note.id !== action.data.id) {
