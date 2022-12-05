66d83e3d0d1cdf31d95e6fd302650292a5a6342e
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-05 20:37:25 +0100

    Part7: Add `React Router` modifications

--- part7/navigation/src/index.js
+++ part7/navigation/src/index.js
@@ -1,5 +1,35 @@
-import { useState } from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+const App = () => {
+  const padding = { padding: 5 };
+
+  return (
+    <Router>
+      <div>
+        <Link style={padding} to="/">
+          home
+        </Link>
+        <Link style={padding} to="/notes">
+          notes
+        </Link>
+        <Link style={padding} to="/users">
+          users
+        </Link>
+      </div>
+
+      <Routes>
+        <Route path="/notes" element={<Notes />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <div>
+        <i>Note app, Department of Computer Science 2022</i>
+      </div>
+    </Router>
+  );
+};
 
 const Home = () => (
   <div>
@@ -18,46 +48,5 @@ const Users = () => (
     <h2>Users</h2>
   </div>
 );
-
-const App = () => {
-  const [page, setPage] = useState("home");
-
-  const toPage = (page) => (event) => {
-    event.preventDefault();
-    setPage(page);
-  };
-
-  const content = () => {
-    if (page === "home") {
-      return <Home />;
-    } else if (page === "notes") {
-      return <Notes />;
-    } else if (page === "users") {
-      return <Users />;
-    }
-  };
-
-  const padding = {
-    padding: 5,
-  };
-
-  return (
-    <div>
-      <div>
-        <a href="" onClick={toPage("home")} style={padding}>
-          home
-        </a>
-        <a href="" onClick={toPage("notes")} style={padding}>
-          notes
-        </a>
-        <a href="" onClick={toPage("users")} style={padding}>
-          users
-        </a>
-      </div>
-
-      {content()}
-    </div>
-  );
-};
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
