409afecc91db2efd6847b95197931a43cad7bb86
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-06 14:33:19 +0100

    Part7: Add `Custom hooks` modifications

---
+++ part7/counter/src/App.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const App = () => {
+  const counter = useCounter();
+
+  return (
+    <div>
+      <div>{counter.value}</div>
+      <button onClick={counter.increase}>plus</button>
+      <button onClick={counter.decrease}>minus</button>
+      <button onClick={counter.zero}>zero</button>
+    </div>
+  );
+};
+
+const useCounter = () => {
+  const [value, setValue] = useState(0);
+
+  const increase = () => {
+    setValue(value + 1);
+  };
+
+  const decrease = () => {
+    setValue(value - 1);
+  };
+
+  const zero = () => {
+    setValue(0);
+  };
+
+  return {
+    value,
+    increase,
+    decrease,
+    zero,
+  };
+};
+export default App;
