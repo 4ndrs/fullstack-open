71dde0327c38ba06538fa0df2457bc516f5bc968
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-11 22:42:43 +0100

    Part8: Add `Making queries` modifications

--- part8/graphql-client/src/App.js
+++ part8/graphql-client/src/App.js
@@ -1,3 +1,34 @@
-const App = () => {};
+import { gql, useQuery } from "@apollo/client";
+const ALL_PERSON = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`;
+const App = () => {
+  const result = useQuery(ALL_PERSON);
+
+  if (result.loading) {
+    return <div>loading...</div>;
+  }
+
+  return <Persons persons={result.data.allPersons} />;
+};
+
+const Persons = ({ persons }) => {
+  return (
+    <div>
+      <h2>Persons</h2>
+      {persons.map((p) => (
+        <div key={p.name}>
+          {p.name} {p.phone}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
