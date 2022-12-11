75987b12de52e1df74a83443d55c7234641db49c
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-11 05:21:37 +0100

    Part8: Add `Changing a phone number` modifications

--- part8/index.js
+++ part8/index.js
@@ -56,6 +56,10 @@ const typeDefs = gql`
       city: String!
     ): Person
   }
+
+  type Mutation {
+    editNumber(name: String!, phone: String!): Person
+  }
 `;
 
 const resolvers = {
@@ -89,6 +93,16 @@ const resolvers = {
       const person = { ...args, id: uuid() };
       persons = persons.concat(person);
       return person;
+    },
+    editNumber: (root, args) => {
+      const person = persons.find((p) => p.name === args.name);
+      if (!person) {
+        return null;
+      }
+
+      const updatedPerson = { ...person, phone: args.phone };
+      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
+      return updatedPerson;
     },
   },
 };
