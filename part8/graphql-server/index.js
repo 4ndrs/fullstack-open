79138084c24281cbf069646fade26410d21c7c36
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-12 09:44:04 +0100

    Part8: Add `Mongoose and Apollo` modifications

--- part8/graphql-server/index.js
+++ part8/graphql-server/index.js
@@ -1,28 +1,21 @@
-import { ApolloServer, UserInputError, gql } from "apollo-server";
-import { v1 as uuid } from "uuid";
+import { ApolloServer, gql } from "apollo-server";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    phone: "040-123543",
-    street: "Tapiolankatu 5 A",
-    city: "Espoo",
-    id: "3d594650-3436-11e9-bc57-8b80ba54c431",
-  },
-  {
-    name: "Matti Luukkainen",
-    phone: "040-432342",
-    street: "Malminkaari 10 A",
-    city: "Helsinki",
-    id: "3d599470-3436-11e9-bc57-8b80ba54c431",
-  },
-  {
-    name: "Venla Ruuska",
-    street: "Nallemaentie 22 C",
-    city: "Helsinki",
-    id: "3d599471-3436-11e9-bc57-8b80ba54c431",
-  },
-];
+import Person from "./models/person.js";
+
+dotenv.config({ path: "./.env.local" });
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+console.log("connecting to ", MONGODB_URI);
+
+try {
+  await mongoose.connect(MONGODB_URI);
+} catch (exception) {
+  console.log("error connecting to MongoDB", exception.message);
+  process.exit(1);
+}
 
 const typeDefs = gql`
   type Address {
@@ -64,16 +57,15 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    personCount: () => persons.length,
-    allPersons: (root, args) => {
+    personCount: async () => Person.collection.countDocuments(),
+    allPersons: async (root, args) => {
       if (!args.phone) {
-        return persons;
+        return Person.find({});
       }
-      const byPhone = (person) =>
-        args.phone === "YES" ? person.phone : !person.phone;
-      return persons.filter(byPhone);
+
+      return Person.find({ phone: { $exists: args.phone === "YES" } });
     },
-    findPerson: (root, args) => persons.find((p) => p.name === args.name),
+    findPerson: async (root, args) => Person.findOne({ name: args.name }),
   },
   Person: {
     address: (root) => {
@@ -84,25 +76,14 @@ const resolvers = {
     },
   },
   Mutation: {
-    addPerson: (root, args) => {
-      if (persons.find((p) => p.name === args.name)) {
-        throw new UserInputError("Name must be unique", {
-          invalidArgs: args.name,
-        });
-      }
-      const person = { ...args, id: uuid() };
-      persons = persons.concat(person);
-      return person;
+    addPerson: async (root, args) => {
+      const person = new Person({ ...args });
+      return person.save();
     },
-    editNumber: (root, args) => {
-      const person = persons.find((p) => p.name === args.name);
-      if (!person) {
-        return null;
-      }
-
-      const updatedPerson = { ...person, phone: args.phone };
-      persons = persons.map((p) => (p.name === args.name ? updatedPerson : p));
-      return updatedPerson;
+    editNumber: async (root, args) => {
+      const person = await Person.findOne({ name: args.name });
+      person.phone = args.phone;
+      return person.save();
     },
   },
 };
@@ -112,6 +93,5 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`);
-});
+const { url } = await server.listen();
+console.log(`Server ready at ${url}`);
