8f4e7d998a13cdd1a39f4575b78a1a5bfc261df6
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2022-12-12 05:54:14 +0100

    Part8: Add `Updating the cache` modifications

--- part8/graphql-client/src/App.js
+++ part8/graphql-client/src/App.js
@@ -1,31 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { useState } from "react";
 
 import PersonForm from "./components/PersonForm";
-
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`;
-
-const FIND_PERSON = gql`
-  query findPersonByName($nameToSearch: String!) {
-    findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
-    }
-  }
-`;
+import { ALL_PERSONS, FIND_PERSON } from "./queries.js";
 
 const App = () => {
   const result = useQuery(ALL_PERSONS);
